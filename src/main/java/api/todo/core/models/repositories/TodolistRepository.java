b62ec4f46222ae2c22e5a4db2fb2533268f2b1e0
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-08-27 18:49:58 +0700

    docs: add method updateTodo no impl query

--- src/main/java/api/todo/core/models/repositories/TodolistRepository.java
+++ src/main/java/api/todo/core/models/repositories/TodolistRepository.java
@@ -1,7 +1,12 @@
 package api.todo.core.models.repositories;
 
 import api.todo.core.models.entities.Todolist;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TodolistRepository extends CrudRepository<Todolist, Long> {
+
+    @Query("")
+    public Todolist updateTodolist();
+
 }
